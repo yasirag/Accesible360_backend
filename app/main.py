d7f7bb1346ed567edbd87c4d980502bb504bd62a
yasirag <yasirag@gmail.com>
2026-08-06 11:20:41 +0200

    fix: add vercel frontend URL to CORS allow_origins

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[  "https://accesible360-frontend-75g1-a1heegzjx-yasirags-projects.vercel.app", "http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
